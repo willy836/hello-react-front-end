93351b0c12be814863e2cc8bf29523ec8b09e423
willy836 <wilsonkimiri836@gmail.com>
2023-05-02 18:09:46 +0300

    Implement Greetings component

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGreeting } from "../redux/greetings/greetingSlice";
 
 const Greetings = () => {
-  return (
-    <section>
-      <p>Greetings component</p>
-    </section>
-  );
+  const { loading, greeting } = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+  const shouldFetchGreeting = useRef(true);
+
+  useEffect(() => {
+    if (shouldFetchGreeting.current) {
+      shouldFetchGreeting.current = false;
+      dispatch(fetchGreeting());
+    }
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  } else {
+    return <p>{greeting.message}</p>;
+  }
 };
 
 export default Greetings;
